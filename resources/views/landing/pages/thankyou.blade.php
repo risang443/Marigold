4ac3dfd73ee3a5ebd6db7392e24739826122cba7
risang443 <risang443@gmail.com>
2024-11-10 21:19:02 +0700

    sudah dulu

--- resources/views/landing/pages/thankyou.blade.php
+++ resources/views/landing/pages/thankyou.blade.php
@@ -1,4 +1,4 @@
-<section class="bg-purple mt-8">
+<section class="bg-purple mt-8 border-b-4 border-b-white">
     <div class="max-w-screen-xl max-h-screen-xl px-4 py-20 mx-auto ">
         <div class="flex justify-center items-center p-32">
             <p class="text-3xl tracking-tight font-['Poppins'] font-semibold text-white mb-10 text-center leading-relaxed ">
