f98faf0bc1b597fff86bfcfff4ec5ec97a4a9132
risang443 <risang443@gmail.com>
2024-11-10 20:22:28 +0700

    sudah dulu

--- resources/views/landing/pages/thankyou.blade.php
+++ resources/views/landing/pages/thankyou.blade.php
@@ -1,7 +1,7 @@
 <section class="bg-purple mt-8">
     <div class="max-w-screen-xl max-h-screen-xl px-4 py-20 mx-auto ">
         <div class="flex justify-center items-center p-32">
-            <p class="text-xl tracking-tight leading-none font-['Poppins'] font-thin text-white mb-10 text-center ">
+            <p class="text-3xl tracking-tight leading-none font-['Poppins'] font-semibold text-white mb-10 text-center ">
                 This work was partially funded by KLN - Vokasi Tematik A3 Grant 2024 - Directorate of Higher Education (Ditlitabmas), Ministry of National Education, Indonesia, and thanks to our Counter partners, from Peter the Great St. Petersburg Polytechnic University (National Research University).</p>
         </div>
     </div>
