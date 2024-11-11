e3e3ab3efa4708e9180672ba020b8ca055f7c3d3
whhidayat8 <whhidayat8@gmail.com>
2024-11-11 13:25:21 +0700

    bebas

--- resources/views/landing/footer.blade.php
+++ resources/views/landing/footer.blade.php
@@ -15,22 +15,31 @@
         </ul>
     </div>
     <div class="">
-        <h1 class="text-white text-lg font-semibold font-['Poppins']">Contact Us</h1>
-    </div>
-    <div class="">
         <h1 class="text-white text-lg font-semibold font-['Poppins']">Product</h1>
-        <ul class="text-white text-sm font-semibold font-['Poppins']">
+        <ul class="text-white text-sm font-['Poppins']">
             <li><a class="hover:underline" href="">Overview</a></li>
         </ul>
     </div>
     <div class="">
         <h1 class="text-white text-lg font-semibold font-['Poppins']">Company</h1>
-        <ul class="text-white text-sm font-semibold font-['Poppins']">
+        <ul class="text-white text-sm font-['Poppins']">
             <li><a class="hover:underline" href="/about">About Us</a></li>
             <li><a class="hover:underline" href="">Blog</a></li>
             <li><a class="hover:underline" href="">Career</a></li>
             <li><a class="hover:underline" href="/faq">FAQ</a></li>
             <li><a class="hover:underline" href="">Media kit</a></li>
+        </ul>
+    </div>
+    <div class="">
+        <h1 class="text-white text-lg font-semibold font-['Poppins']">Contact Us</h1>
+        <ul class="text-white text-sm font-['Poppins']">
+            <div class="flex flex-row items-center gap-3">
+                <i class="fi fi-rr-envelope"></i>
+                <li><a class="hover:underline" href="">ugmarigoldlaris@gmail.com</a></li>
+            </div>
+            
+            <li><a class="hover:underline" href="">+62 0000 0000 0000</a></li>
+            <li><a class="hover:underline" href="">Jl. Margonda Raya Pondok Cina, Depok Phone : 7863819, 7520981,7863788</a></li>
         </ul>
     </div>
 </div>
