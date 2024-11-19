210eb117d16000259d67321c5c335be6deb5a6be
whhidayat8 <whhidayat8@gmail.com>
2024-11-19 20:15:12 +0700

    bebas

--- resources/views/landing/footer.blade.php
+++ resources/views/landing/footer.blade.php
@@ -6,23 +6,26 @@
     </div>
     <div class="">
         <h1 class="text-white text-lg font-semibold font-['Poppins']">Sosial Media</h1>
-        <ul class="text-white text-sm font-semibold font-['Poppins']">
-            <li>
-                <a class="hover:underline" href="https://www.instagram.com/ug_marigold/profilecard/?igsh=cGNoc3htMG00Zzhp">
-                    <i class="fi fi-brands-instagram"></i>Instagram
-                </a>
-            </li>
-        </ul>
+        <div>
+            <ul class="text-white text-sm font-semibold font-['Poppins']">
+                <li>
+                    <a class="hover:underline" href="https://www.instagram.com/ug_marigold/profilecard/?igsh=cGNoc3htMG00Zzhp">
+                        <i class="fi fi-brands-instagram"></i>Instagram
+                    </a>
+                </li>
+            </ul>
+        </div>
+
     </div>
     <div class="">
-        <h1 class="text-white text-lg font-semibold font-['Poppins']">Product</h1>
+        <h1 class="text-white text-lg font-semibold font-['Poppins'] mb-3">Product</h1>
         <ul class="text-white text-sm font-['Poppins']">
             <li><a class="hover:underline" href="">Overview</a></li>
         </ul>
     </div>
     <div class="">
-        <h1 class="text-white text-lg font-semibold font-['Poppins']">Company</h1>
-        <ul class="text-white text-sm font-['Poppins']">
+        <h1 class="text-white text-lg font-semibold font-['Poppins'] mb-3">Company</h1>
+        <ul class="text-white text-sm font-['Poppins'] flex flex-col gap-2">
             <li><a class="hover:underline" href="/about">About Us</a></li>
             <li><a class="hover:underline" href="">Blog</a></li>
             <li><a class="hover:underline" href="">Career</a></li>
@@ -31,15 +34,20 @@
         </ul>
     </div>
     <div class="">
-        <h1 class="text-white text-lg font-semibold font-['Poppins']">Contact Us</h1>
-        <ul class="text-white text-sm font-['Poppins']">
+        <h1 class="text-white text-lg font-semibold font-['Poppins'] mb-3">Contact Us</h1>
+        <ul class="text-white text-sm font-['Poppins'] flex flex-col gap-2">
             <div class="flex flex-row items-center gap-3">
                 <i class="fi fi-rr-envelope"></i>
                 <li><a class="hover:underline" href="">ugmarigoldlaris@gmail.com</a></li>
             </div>
-            
-            <li><a class="hover:underline" href="">+62 0000 0000 0000</a></li>
-            <li><a class="hover:underline" href="">Jl. Margonda Raya Pondok Cina, Depok Phone : 7863819, 7520981,7863788</a></li>
+            <div class="flex flex-row items-center gap-3">
+                <i class="fi fi-rr-phone-flip"></i>
+                <li><a class="hover:underline" href="">+62 0000 0000 0000</a></li>
+            </div>
+            <div class="flex flex-row items-center gap-3">
+                <i class="fi fi-rr-building"></i>
+                <li><a class="hover:underline" href="">Jl. Margonda Raya Pondok Cina, Depok Phone : 7863819, 7520981,7863788</a></li>
+            </div>
         </ul>
     </div>
 </div>
