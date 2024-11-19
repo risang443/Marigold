9a72a243c9ab362938657f9da2c00080837387e6
whhidayat8 <whhidayat8@gmail.com>
2024-11-19 21:52:08 +0700

    tanggal19

--- resources/views/landing/footer.blade.php
+++ resources/views/landing/footer.blade.php
@@ -11,43 +11,68 @@
         <div>
           <h1 class="text-white text-lg font-semibold font-['Poppins']">Social Media</h1>
           <ul class="text-white text-sm font-semibold font-['Poppins']">
-            <li>
-              <a
-                class="hover:underline flex items-center gap-2"
-                href="https://www.instagram.com/ug_marigold/profilecard/?igsh=cGNoc3htMG00Zzhp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i class="fi fi-brands-instagram"></i>
-                Instagram
-              </a>
-            </li>
+            <div class="flex flex-col gap-2 mt-3">
+                <li>
+                <a
+                    class="hover:underline flex items-center gap-2"
+                    href="https://www.instagram.com/ug_marigold/profilecard/?igsh=cGNoc3htMG00Zzhp"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <i class="fi fi-brands-instagram"></i>
+                    Instagram
+                </a>
+                </li>
+            </div>
           </ul>
-        </div>
-  
-        <!-- Contact Us Section -->
-        <div>
-          <h1 class="text-white text-lg font-semibold font-['Poppins']">Contact Us</h1>
-          <!-- Add contact details here -->
         </div>
   
         <!-- Product Section -->
         <div>
           <h1 class="text-white text-lg font-semibold font-['Poppins']">Product</h1>
-          <ul class="text-white text-sm font-semibold font-['Poppins']">
-            <li><a class="hover:underline" href="">Overview</a></li>
+          <ul class="text-white text-sm font-['Poppins']">
+            <div class="flex flex-col gap-2 mt-3">
+                <li><a class="hover:underline" href="">Overview</a></li>
+            </div>
         </ul>
     </div>
     <div class="">
         <h1 class="text-white text-lg font-semibold font-['Poppins']">Company</h1>
-        <ul class="text-white text-sm font-semibold font-['Poppins']">
-            <li><a class="hover:underline" href="/about">About Us</a></li>
-            <li><a class="hover:underline" href="">Blog</a></li>
-            <li><a class="hover:underline" href="">Career</a></li>
-            <li><a class="hover:underline" href="/faq">FAQ</a></li>
-            <li><a class="hover:underline" href="">Media kit</a></li>
+        <ul class="text-white text-sm font-['Poppins']">
+            <div class="flex flex-col gap-2 mt-3">
+                <li><a class="hover:underline" href="/about">About Us</a></li>
+                <li><a class="hover:underline" href="">Blog</a></li>
+                <li><a class="hover:underline" href="">Career</a></li>
+                <li><a class="hover:underline" href="/faq">FAQ</a></li>
+                <li><a class="hover:underline" href="">Media kit</a></li>
+            </div>
         </ul>
     </div>
+        <!-- Contact Us Section -->
+         <div>
+            <h1 class="text-white text-lg font-semibold font-['Poppins']">Contact Us</h1>
+            <!-- Add contact details here -->
+            <ul class="text-white text-sm font-['Poppins']">
+                <div class="flex flex-col gap-2 mt-3">
+                    <div class="flex flex-row gap-4">
+                        <i class="fi fi-rr-envelope"></i>
+                        <li><a class="hover:underline" href="/about">ugmarigoldlaris@gmail.com</a></li>
+                    </div>
+                    <div class="flex flex-row gap-4">
+                        <i class="fi fi-rr-phone-flip"></i>
+                        <li><a class="hover:underline" href="">+62 0000 0000 0000</a></li>
+                    </div>
+                    <div class="flex flex-row gap-4">
+                        <i class="fi fi-rr-building"></i>
+                        <li><a class="hover:underline" href="">Jl. Margonda Raya Pondok Cina, Depok Phone : 7863819, 7520981,7863788</a></li>
+                    </div>
+                </div>
+                
+                
+                
+            </ul>
+            </div>
+        
 </div>
 <hr>
 <div class="flex justify-center items-center">
